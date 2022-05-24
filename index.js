67c2ba0307d0e694af23112143f4d3172e9c5e06
Jorge Alcala <58161878+jorge1alcala@users.noreply.github.com>
2022-05-23 19:10:21 -0700

    correcting small things

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-const express = require('express');
-const fs = require('fs');
-const bodyParser = require('body-parser');
-const ejs = require('ejs');
-const _ = require('lodash');
+const express = require("express");
+const fs = require("fs");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
 const app = express();
 
 // Fetch posts from API
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 // Settins
-app.set('port', process.env.PORT || 3000);
-app.set('view engine', 'ejs');
+app.set("port", process.env.PORT || 3000);
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Routs
-app.get('/', (req, res) => {
-  res.render('overview', { data: dataObj });
+app.get("/", (req, res) => {
+  res.render("overview", { data: dataObj });
 });
 
-app.get('/products/:prodID', (req, res) => {
+app.get("/products/:prodID", (req, res) => {
   const idUrl = _.lowerCase(req.params.prodID);
 
   dataObj.forEach((item) => {
     const id = _.lowerCase(item.id);
 
     if (idUrl === id) {
-      res.render('product', {
+      res.render("product", {
         productName: item.productName,
         from: item.from,
         nutrients: item.nutrients,
@@ -35,13 +35,13 @@ app.get('/products/:prodID', (req, res) => {
         price: item.price,
         organic: item.organic,
         description: item.description,
-        image: item.image,
+        image: item.image
       });
     }
   });
 });
 
 // Startiring Sever
-app.listen(app.get('port'), () =>
-  console.log(`Port is Running on port: ${app.get('port')}`)
+app.listen(app.get("port"), () =>
+  console.log(`Port is Running on port: ${app.get("port")}`)
 );
